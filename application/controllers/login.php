1534d3e4c1a43769b9c67378e361dcf11c672080
nawalhadiah <balqis.sulaiman.18@gmail.com>
2017-02-08 11:51:01 +0800

    login
    17012017

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -1,11 +1,69 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 class Login extends CI_Controller {
-	function index ()
-	{
+	function index (){
 		 $data['main_content'] ='login_form';
+		 //$this->load->view('nastyHRMS/example/login', $data);
 		 $this->load->view('include/templete', $data);
 	}
 
+	function __construct(){
+		parent::__construct();
+		$this->load->library('session', 'my_func');
+	}
+	//sql injection alert ***
+	    function signin()
+	    {
+	    	 //$this->load->library("encrypt");
+	    	 $email = $this->input->post('email');
+	    	 $pass = $this->input->post('pass');
 
-}
+	    	 $this->load->model('m_login');
+	    	 $data = $this->m_login->login($email,$pass);
+	    	
+	    	 if ($data) 
+	    	 {
+	    	 	/*echo "login Success";*/
+	    	 /*	$array = array(
+	    	 		'user_id' => $this->my_func->nastyHRMS_encrypt($data->user_id),
+	    	 		'user_name' => $this->my_func->nastyHRMS_encrypt($data->user_name)
+	    	 	);	  */  		
+	    	 	//$this->session->set_userdata( $array );
+	    		redirect(base_url('index.php'),'refresh');
+	    	 }
+	    	 else
+	    	 {
+	    	 	echo "Login Not Success";
+	    	 	redirect(site_url('login'),'refresh');
+	    	 }
+	    	//$temp = $this->m_login->get();
+	    	/*echo "<pre>";
+	    	print_r($data);
+	    	echo "</pre>";*/
+	   }
+
+	    function signup()
+	    {
+	    	$email = $this->input->post("email");
+	        $pass = $this->input->post("pass");
+
+	        $this->load->library("my_func");
+	        $pass = $this->my_func->nastyHRMS_encrypt($pass);
+
+	        //echo $email;
+	        //echo $pass;
+
+	        $this->load->model("m_login");
+
+	        $data = array(
+	        	"user_email" => $email , 
+	        	"user_password" => $pass
+	        );
+	        if (!$this->m_login->insert($data)) {
+	        	echo "Not success";
+	        }else{
+	        	echo "success";
+	        }
+	    }
+	}
+
--- application/views/login_form.php
+++ application/views/login_form.php
@@ -1,10 +1,108 @@
-<div id="login_form">
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Nasty H.R.M.S</title>
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  <!-- Bootstrap 3.3.6 -->
+  <link rel="stylesheet" href="<?php echo base_url();?>/bootstrap/css/bootstrap.min.css">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="<?php echo base_url();?>/dist/css/AdminLTE.min.css">
+  <!-- iCheck -->
+  <link rel="stylesheet" href="<?php echo base_url();?>/plugins/iCheck/square/blue.css">
+
+  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
+</head>
+<body class="hold-transition login-page">
+
+<div class="login-box">
+  <div  align="center">
+     <img src="<?php echo base_url();?>/asset/nasty.png" class="img-responsive" alt="Image" align="center">
+  </div>
+   <div class="clear" style="height:20px;"></div>
+   <div class="clear" style="height:20px;"></div>
+ 
+  <!-- /.login-logo -->
+  <div class="login-box-body">
+ 
+    <h3 align="center"  class="login-box-msg"><b>User</b> Login</h3>
+    
+
+    <form action="<?= site_url('login/signin'); ?>" method="post">
+      <div class="form-group has-feedback">
+        <input type="email" class="form-control" placeholder="Email" name="email" id="email">
+        <span class="glyphicon glyphicon-user form-control-feedback"></span>
+      </div>
+      <div class="form-group has-feedback">
+        <input type="password" class="form-control" placeholder="Password" name="pass" id="pass">
+        <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+      </div>
+      <div class="row">
+        <div class="col-xs-8">
+          <div class="checkbox icheck">
+            <label>
+              <input type="checkbox"> Remember Me
+            </label>
+          </div>
+        </div>
+        <!-- /.col -->
+        <div class="col-xs-4">
+          <button type="submit" class="btn btn-primary btn-block btn-flat"><i class="fa fa-unlock-alt"></i>&nbsp;&nbsp;&nbsp;Login</button>
+        </div>
+        <!-- /.col -->
+      </div>
+    </form>
+
+   
+
+    <a href="#">I forgot my password</a><br>
+    <a href="register.html" class="text-center">Register a new membership</a>
+
+  </div>
+  <!-- /.login-box-body -->
+</div>
+<!-- /.login-box -->
+
+<!-- jQuery 2.2.3 -->
+<script src="<?php echo base_url();?>/plugins/jQuery/jquery-2.2.3.min.js"></script>
+<!-- Bootstrap 3.3.6 -->
+<script src="<?php echo base_url();?>/bootstrap/js/bootstrap.min.js"></script>
+<!-- iCheck -->
+<script src="<?php echo base_url();?>/plugins/iCheck/icheck.min.js"></script>
+<script>
+  $(function () {
+    $('input').iCheck({
+      checkboxClass: 'icheckbox_square-blue',
+      radioClass: 'iradio_square-blue',
+      increaseArea: '20%' // optional
+    });
+  });
+</script>
+</body>
+</html>
+
+
+
+
+<!-- <div id="login_form">
 	<h1>Login Here</h1>
 	<?php 
 	echo form_open('login/validate_credentials');
 	echo form_input('username','Username');
-	echo form_inpit('password','Password');
+	echo form_input('password','Password');
 	echo form_submit('submit','Login');
 	echo anchor('login/signup','Create Account');
 	?>
 </div>
+ -->
